33e4cdfdf03022a8e2ade8e03370030117021895
nin0dev <personal@nin0.dev>
2024-10-15 05:19:25 -0400

    feat: typescript

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
 import { WebSocketServer } from "ws";
+import { ChatClient, Role } from "./utils/types";
+import { config } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws: ChatClient) {
     ws.on("error", console.error);
 
     ws.on("message", function message(data) {
         console.log("received: %s", data);
     });
 
-    ws.send("something");
+    // Initialise client and send system message
+    ws.role = Role.Guest;
+    ws.send(
+        JSON.stringify({
+            op: 0,
+            d: {
+                userInfo: {
+                    username: "System",
+                    role: Role.Admin
+                },
+                content:
+                    "Welcome to nin0chat! You are currently connected as an unauthenticated guest and cannot talk until you either login or set your username."
+            }
+        })
+    );
 });
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,16 +1,16 @@
 import { WebSocket } from "ws";
 
-enum Role {
-    Guest = -1,
-    User = 0,
-    Embedder = 1,
-    Bot = 2,
-    Mod = 10,
-    Admin = 20
-}
+export const Role = {
+    Guest: -1,
+    User: 0,
+    Embedder: 1,
+    Bot: 2,
+    Mod: 10,
+    Admin: 20
+};
 
 export interface ChatClient extends WebSocket {
     id?: number;
-    role?: Role;
+    role?: number;
     username?: string;
 }
